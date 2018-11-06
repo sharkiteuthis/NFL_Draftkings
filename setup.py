270d33103ce65a18c8ff113fc5edc21e230f741d
Tom Dodson <tcdodson@gmail.com>
2018-11-05 22:57:11 -0500

    update to allow pip install with requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 setup(
-  name = 'NFL_Draftkings',
-  packages = ['NFL_Draftkings'],
-  version = '3',
-  description = 'Python wrapper around NFL api that calculates Draftkings scores for players',
-  author = 'Kacper Adach',
-  author_email = 'kacperadach@gmail.com',
-  url = 'https://github.com/kacperadach/NFL_Draftkings',
-  download_url = 'https://github.com/kacperadach/NFL_Draftkings/tarball/2',
-  keywords = ['nfl', 'draftkings', 'python', 'api', 'wrapper', 'player', 'scores', 'points'],
-  classifiers = [],
+  name='NFL_Draftkings',
+  packages=['NFL_Draftkings'],
+  version='3',
+  description='Python wrapper around NFL api that calculates Draftkings scores for players',
+  author='Tom Dodson',
+  author_email='tcdodson@gmail.com',
+  url='https://github.com/sharkiteuthis/NFL_Draftkings',
+  download_url='https://github.com/sharkiteuthis/NFL_Draftkings/tarball/2',
+  keywords=['nfl', 'draftkings', 'python', 'api', 'wrapper', 'player', 'scores', 'points'],
+  classifiers=[],
+  install_requires=['requests-cache'],
 )
